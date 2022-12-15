38e39f4731893065a887a10c63b0274cba12d710
Athena Serene <aserenehoosang@gmail.com>
2022-12-14 18:56:45 -0800

    tic tac toe design made

---
+++ src/components/Board.js
@@ -0,0 +1,9 @@
+import Square from "./Square"
+function Board(props) {
+    return (
+        <div className="board">
+          <Square/><Square/><Square/><Square/><Square/><Square/><Square/><Square/><Square/>
+        </div>
+      )
+  }
+export default Board
---
+++ src/components/Square.js
@@ -0,0 +1,11 @@
+function Square (props) {
+    return (
+        <div>
+            <h4>
+                Square
+            </h4>
+        </div>
+    )
+}
+
+export default Square
